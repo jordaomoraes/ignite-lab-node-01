b8c92b5bbc8be2abc3b8ced845ee1c155c87a0e4
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-12-14 17:04:03 -0300

    chore(aplication): configurando pastas, adicionado tests unitarios

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,58 @@
+/* eslint-disable prettier/prettier */
+
+import { Replace } from 'src/helpers/replace';
+import { Content } from './content';
+
+export interface NotificationProps {
+  recipientID: string;
+  content: Content;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+  public set recipientID(recipientID: string) {
+    this.props.recipientID = recipientID;
+  }
+
+  public get recipientID(): string {
+    return this.props.recipientID;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
